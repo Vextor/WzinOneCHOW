16a46a2e1968962c18c2df92fc25f4177356e46f
Vextor <vexter@outlook.com>
2013-04-23 18:47:15 +0100

    Fail

---
+++ WzinOneCHOW/MW2ModernBanList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WzinOneCHOW
+{
+    public partial class MW2ModernBanList : Form
+    {
+        public MW2ModernBanList()
+        {
+            InitializeComponent();
+        }
+    }
+}
